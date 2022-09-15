238235f89ea16be4a29f70d1fad9ac80770e1305
bruno antunes <68615491+BrunoAntunes21@users.noreply.github.com>
2022-09-15 17:55:54 -0300

    Por que me odeias laravel?

--- app_super_gestao7.0.0/database/migrations/2022_09_02_191913_alter_table_site_contatos_add_fk_motivo_contatos.php
+++ app_super_gestao7.0.0/database/migrations/2022_09_02_191913_alter_table_site_contatos_add_fk_motivo_contatos.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AlterTableSiteContatosAddFkMotivoContatos extends Migration
@@ -13,18 +14,25 @@ class AlterTableSiteContatosAddFkMotivoContatos extends Migration
      */
     public function up()
     {   //adicionando o a coluna motivo_contatos_id na tabela motivo_contato
-        Schema::create('motivo_contatos',function(Blueprint $table){
+       /* Schema::table('motivo_contatos',function(Blueprint $table){
            $table->unsignedBigInteger('motivo_contatos_id');
 
 
-        });
+        });*/
         //permite que seja executada qualquer query no banco de dados
-        DB::statement('update site_contatos set motivo_contatos_id = motivo_contato');
+
         //criando a fk e removendo a coluna motivo contato
 
         Schema::table('site_contatos',function (Blueprint $table){
-        $table->foreign('motivo_contatos_id')->references('id')->on('motivo_contatos');
-        $table->dropColumn('motivo_contato');});
+           $table->foreign('motivo_contatos_id')->references('id')->on('motivo_contatos');
+            $table->dropColumn('motivo_contato');}
+        );
+
+
+
+
+
+            DB::statement('update site_contatos set motivo_contatos_id = motivo_contato');
     }
 
 
@@ -36,7 +44,7 @@ class AlterTableSiteContatosAddFkMotivoContatos extends Migration
     public function down()
     {
         DB::statement('update site_contatos set motivo_contatos = motivo_contato_id');
-        
+
         Schema::table('site_contato',function(Blueprint $table){
             $table->interger('motivo_contato');
             $table->dropForeign('site_contato_contato<coluna>_foreign');
